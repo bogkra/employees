ec7ec2c420bdf6de79bbefa5f596f50a2fd51c2c
lukasz99933 <lukasz8126700@gmail.com>
2018-02-06 16:16:40 +0100

    Poprawka 1

--- main.cpp
+++ main.cpp
@@ -6,11 +6,15 @@ int
 main()
 {
   vector<Person*> person;
-  Worker w("Siema", "S", "i", "k", 120);
-  Student s("Wojtek", "W", "l", "q", 226);
+  Worker w("Siema", "S", "i", female, "k", 120);
+  Student s("Wojtek", "W", "l", female, "q", 226);
   Person* wsk;
   wsk = &w;
   person.push_back(wsk);
-  cout << person[0]->number() << endl;
+  wsk = &s;
+  person.push_back(wsk);
+  cout << person[0]->occupation() << person[0]->number() << person[0]->sex
+       << endl;
+  cout << person[1]->occupation() << person[1]->number() << endl;
   return 0;
 }
--- main.h
+++ main.h
@@ -1,5 +1,6 @@
 #ifndef main_h
 #define main_h
+#include <string>
 
 using namespace std;
 
@@ -11,23 +12,30 @@ using namespace std;
     float pay;
   };
 */
-// enum Sex {female, male, mysterious };
+enum Sex
+{
+  female,
+  male,
+  mysterious
+};
 
 class Person
 {
 public:
-  Person(string firstName, string name, string pesel, string address)
+  Person(string firstName, string name, string pesel, Sex sex, string address)
     : firstName(firstName)
     , name(name)
     , pesel(pesel)
+    , sex(sex)
     , address(address)
   {
   }
   string firstName;
   string name;
   string pesel;
-  // Sex sex;
+  Sex sex;
   string address;
+  virtual string occupation() = 0;
   virtual int number() = 0;
 };
 
@@ -37,26 +45,36 @@ public:
   Student(string firstName,
           string name,
           string pesel,
+          Sex sex,
           string address,
           int number_index)
-    : Person(firstName, name, pesel, address)
+    : Person(firstName, name, pesel, sex, address)
     , number_index(number_index)
   {
   }
   int number_index;
+  string student = "Student";
   int number() { return number_index; }
+  string occupation() { return student; }
 };
 
 class Worker : public Person
 {
 public:
-  Worker(string firstName, string name, string pesel, string address, int pay)
-    : Person(firstName, name, pesel, address)
+  Worker(string firstName,
+         string name,
+         string pesel,
+         Sex sex,
+         string address,
+         int pay)
+    : Person(firstName, name, pesel, sex, address)
     , pay(pay)
   {
   }
   int pay;
+  string worker = "Worker";
   int number() { return pay; }
+  string occupation() { return worker; }
 };
 
 #endif
