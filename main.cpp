9fc6d02cdf0f6103db6a530de078d22304628cd4
lukasz99933 <lukasz8126700@gmail.com>
2018-02-06 15:02:26 +0100

    Poprawa formatowania.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
+#include "main.h"
 #include <iostream>
 #include <vector>
-#include "main.h"
 
-int main ()
+int
+main()
 {
-	vector<Person* > person;
-	Worker w("Siema","S","i","k",120);
-	Student s("Wojtek","W","l","q",226);
-	Person *wsk;
-	wsk=&w;
-	person.push_back(wsk);
-	cout << person[0]->number() <<endl;
+  vector<Person*> person;
+  Worker w("Siema", "S", "i", "k", 120);
+  Student s("Wojtek", "W", "l", "q", 226);
+  Person* wsk;
+  wsk = &w;
+  person.push_back(wsk);
+  cout << person[0]->number() << endl;
   return 0;
 }
--- main.h
+++ main.h
@@ -3,60 +3,60 @@
 
 using namespace std;
 
-//typedef char Index[30];
-//typedef long int Index;
+// typedef char Index[30];
+// typedef long int Index;
 
 /*union ExtraData {
     Index index;
     float pay;
   };
 */
-//enum Sex {female, male, mysterious };
-
+// enum Sex {female, male, mysterious };
 
 class Person
 {
-  public:
-  Person(string firstName, string name, string pesel, string address) :
-	  firstName(firstName),
-	  name(name),
-	  pesel(pesel),
-	  address(address) 
-  {}
+public:
+  Person(string firstName, string name, string pesel, string address)
+    : firstName(firstName)
+    , name(name)
+    , pesel(pesel)
+    , address(address)
+  {
+  }
   string firstName;
   string name;
   string pesel;
-  //Sex sex;
+  // Sex sex;
   string address;
-  virtual int number()=0;
+  virtual int number() = 0;
 };
 
 class Student : public Person
 {
-	public:
- Student(string firstName, string name, string pesel, string address, int number_index) :
-	 Person(firstName,name,pesel,address),
-	 number_index(number_index)
- {}
- int number_index;
- int number()
- {
-	return number_index;	
- }
+public:
+  Student(string firstName,
+          string name,
+          string pesel,
+          string address,
+          int number_index)
+    : Person(firstName, name, pesel, address)
+    , number_index(number_index)
+  {
+  }
+  int number_index;
+  int number() { return number_index; }
 };
 
 class Worker : public Person
 {
-	public:
-	Worker(string firstName, string name, string pesel, string address, int pay) :
-	 Person(firstName,name,pesel,address),
-	 pay(pay)
- {}
-	int pay;
- int number()
- {
-	return pay;
- }
+public:
+  Worker(string firstName, string name, string pesel, string address, int pay)
+    : Person(firstName, name, pesel, address)
+    , pay(pay)
+  {
+  }
+  int pay;
+  int number() { return pay; }
 };
 
 #endif
