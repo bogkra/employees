576683e29700ab1c6bfd9665f9ddd98b127b2f84
lukasz99933 <lukasz8126700@gmail.com>
2018-02-12 10:30:31 +0100

    Add find person with pesel and surname.

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ using namespace std;
 void
 show(vector<Person*> m)
 {
+  cout << "Database:" << endl;
   int i = 0;
   for (auto it = m.begin(); it != m.end(); ++it) {
     cout << m[i]->occupation() << "  " << m[i]->firstName << "  " << m[i]->name
@@ -16,6 +17,47 @@ show(vector<Person*> m)
          << "  " << m[i]->number() << endl;
     i++;
   }
+  cout <<endl;
+}
+
+void
+findPersonpesel(string peselNumber, vector<Person*>& m)
+{ 	
+  auto it2 = m.begin();
+  int i = 0;
+  for (auto it = m.begin(); it != m.end(); it++) {
+    if (peselNumber == m[i]->pesel) {
+      cout << "Found Person:" << endl;
+      cout << m[i]->occupation() << "  " << m[i]->firstName << "  " << m[i]->name
+         << "  " << m[i]->pesel << "  " << m[i]->sex << "  " << m[i]->address
+         << "  " << m[i]->number() << endl;
+      return;
+    }
+    if (peselNumber != m[i]->pesel) {
+      i++;
+    }
+  }
+  cout << "Lack of pesel!" << endl;
+}
+
+void
+findPersonsurname(string surname, vector<Person*>& m)
+{ 	
+  auto it2 = m.begin();
+  int i = 0;
+  for (auto it = m.begin(); it != m.end(); it++) {
+    if (surname == m[i]->name) {
+      cout << "Found Person:" << endl;
+      cout << m[i]->occupation() << "  " << m[i]->firstName << "  " << m[i]->name
+         << "  " << m[i]->pesel << "  " << m[i]->sex << "  " << m[i]->address
+         << "  " << m[i]->number() << endl;
+      return;
+    }
+    if (surname != m[i]->name) {
+      i++;
+    }
+  }
+  cout << "Lack of surname!" << endl;
 }
 
 void
@@ -66,6 +108,8 @@ main()
   show(person);
   deletePerson("12345667", person);
   show(person);
+  findPersonpesel("89000000",person);
+  findPersonsurname("Witkowik",person);
   delete wsk;
   delete wsk1;
   return 0;
